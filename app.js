5369117c15846c1cbad4882f1824dc29e8de38a7
BackOffice Soluções <contato@backofficesolucoes.io>
2020-08-19 02:17:59 -0300

    updates

---
+++ app.js
@@ -0,0 +1,25 @@
+// set up ======================================================================
+const hostname = '127.0.0.1';
+const port = 3000;
+const express = require('express')
+const bodyParser = require('body-parser');
+const router = require('./routes/web');
+const app = express();
+const fs = require('fs');
+
+try {
+
+    app.listen(port, hostname, (req, res) => {
+        console.log(`Server running at http://${hostname}:${port}/`);
+    });
+
+    app.use( bodyParser.json() );       // to support JSON-encoded bodies
+    app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+        extended: true
+    }));
+        
+    app.use('/', router);
+
+} catch (error) {
+    return JSON.stringify({'error' : 'dont load server', 'infos': error});
+}
---
+++ routes/web.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const sinespApi = require('sinesp-api');
+const rateLimit = require("express-rate-limit");
+const router = express.Router();
+const  app = express();
+
+const createAccountLimiter = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 hour window
+    max: 5, // start blocking after 5 requests
+    message: JSON.stringify({'error' : 'Too many accounts created from this IP, please try again after an hour', 'infos': 'null'})
+});
+
+async function getPlate(plate) {
+    return await sinespApi.search(plate);
+}
+
+router.post('/plate/sinesp', createAccountLimiter, async(req, res) => 
+{
+
+    let plate = req.body.plate;
+    if(typeof(plate) !== 'undefined'){
+        try {
+            console.log(req.body.plate)
+            let response = await getPlate(plate)
+            res.end(JSON.stringify(response));
+        
+        } catch (error) {
+            res.end(JSON.stringify({'error' : 'don\'t have informations from vehicle plate', 'plate' : req.params.key, 'infos': error}));
+        }
+
+    }else{
+        res.end(JSON.stringify({'error' : 'please, insert plate valid', 'plate':'null','infos':'null'}));
+    }
+    
+})
+
+module.exports = router;
